0c2391d1aa6422b6e06e0131885d5d52fac6f36c
Priya-shan <Shanmugapriyashanu2002@gmail.com>
2021-09-29 06:38:07 +0530

    finalCommit

--- LGM-Task 1/singlepagewebsite/script.js
+++ LGM-Task 1/singlepagewebsite/script.js
@@ -1,1 +1,8 @@
 /* this is script file  */
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.carousel');
+    M.Carousel.init(elems, {
+        fullWidth: true,
+        indicators: true
+    });
+});
